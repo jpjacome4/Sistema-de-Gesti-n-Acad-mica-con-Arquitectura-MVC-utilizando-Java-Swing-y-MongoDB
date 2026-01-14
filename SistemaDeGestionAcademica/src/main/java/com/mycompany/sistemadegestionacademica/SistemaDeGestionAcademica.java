57c40c20c1f2963a1cd66aecd5bf05d00f557cdd
Jonathan_Jacome <jpjacome4@espe.edu.ec>
2026-01-14 18:41:56 -0500

    Proyecto Final

---
+++ SistemaDeGestionAcademica/src/main/java/com/mycompany/sistemadegestionacademica/SistemaDeGestionAcademica.java
@@ -0,0 +1,9 @@
+package com.mycompany.sistemadegestionacademica;
+
+
+public class SistemaDeGestionAcademica {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
