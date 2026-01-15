225644b9f7722e829ce53f76f9f79c9994f37eea
Jonathan_Jacome <jpjacome4@espe.edu.ec>
2026-01-14 20:09:17 -0500

    Archivo main

--- src/main/java/com/mycompany/sistemadegestionacademica/SistemaDeGestionAcademica.java
+++ src/main/java/com/mycompany/sistemadegestionacademica/SistemaDeGestionAcademica.java
@@ -4,6 +4,6 @@ package com.mycompany.sistemadegestionacademica;
 public class SistemaDeGestionAcademica {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hola mundo");
     }
 }
