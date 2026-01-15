eb23fffb88657fd724edc052bb417610cf3ba55d
Jonathan_Jacome <jpjacome4@espe.edu.ec>
2026-01-14 20:02:26 -0500

    Configuración del proyecto (Maven)

--- src/main/java/com/mycompany/sistemadegestionacademica/SistemaDeGestionAcademica.java
+++
@@ -1,9 +0,0 @@
-package com.mycompany.sistemadegestionacademica;
-
-
-public class SistemaDeGestionAcademica {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
