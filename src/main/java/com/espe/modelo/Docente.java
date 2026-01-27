19395c2d3c9265b01d6f7b3cba11d888cb16bcb1
Jonathan_Jacome <jpjacome4@espe.edu.ec>
2026-01-26 20:46:32 -0500

    Usuario docente

--- src/main/java/com/espe/modelo/Docente.java
+++ src/main/java/com/espe/modelo/Docente.java
@@ -8,6 +8,36 @@ package com.espe.modelo;
  *
  * @author Paul
  */
-public class Docente {
+public class Docente extends Usuario{
+    public String titulo;
+    public String departamento;
+     
+    public Docente(String cedula, String nombre, String apellido, String correo, String titulo, String departamento) {
+        super(cedula, nombre, apellido, correo);
+        this.titulo = titulo;
+        this.departamento = departamento;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDepartamento() {
+        return departamento;
+    }
+
+    public void setDepartamento(String departamento) {
+        this.departamento = departamento;
+    }
+    
+
+    @Override
+    public String toString() {
+        return super.toString() + ", titulo=" + titulo + ", especialidad=" + departamento + '}';
+    }
     
 }
