b995deae71340134307d7c27ce959c0fd2d238e1
Jonathan_Jacome <jpjacome4@espe.edu.ec>
2026-01-26 20:45:31 -0500

    Usuario Estudiante

--- src/main/java/com/espe/modelo/Estudiante.java
+++ src/main/java/com/espe/modelo/Estudiante.java
@@ -8,6 +8,27 @@ package com.espe.modelo;
  *
  * @author Paul
  */
-public class Estudiante {
+public class Estudiante extends Usuario{
+    private String carrera;
     
+    public Estudiante(String cedula, String nombre, String apellido, String correo, String carrera) {
+        super(cedula, nombre, apellido, correo);
+        this.carrera = carrera;
+    }
+
+    public String getCarrera() {
+        return carrera;
+    }
+
+    public void setCarrera(String carrera) {
+        this.carrera = carrera;
+    }
+
+    @Override
+    public String toString() {
+        // Reutilizamos el toString() de Usuario y le sumamos la carrera
+        return super.toString() + ", carrera=" + carrera + '}';
+    }
 }
+   
+    
