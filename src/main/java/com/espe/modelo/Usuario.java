d8abdf16eb0b5cf792fd285b949dee7154ae0ec1
Jonathan_Jacome <jpjacome4@espe.edu.ec>
2026-01-26 20:44:14 -0500

    Usuario clase abstracta

--- src/main/java/com/espe/modelo/Usuario.java
+++ src/main/java/com/espe/modelo/Usuario.java
@@ -9,5 +9,42 @@ package com.espe.modelo;
  * @author Paul
  */
 public abstract class Usuario {
+    private String cedula;
+    private String nombre;
+    private String apellido;
+    private String correo;
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
     
+    public String toString;
 }
