76b16a39260f8fba4632e768f302dcf16024608c
aatorres26 <aatorres26@espe.edu.ec>
2026-02-02 14:17:34 -0500

    Persona_3

--- src/main/java/com/espe/dao/MatriculaDAO.java
+++ src/main/java/com/espe/dao/MatriculaDAO.java
@@ -4,10 +4,66 @@
  */
 package com.espe.dao;
 
+import com.espe.interfaces.ICrud;
+import com.espe.modelo.Matricula;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import java.util.ArrayList;
+import java.util.List;
+import org.bson.Document;
+
 /**
  *
  * @author Paul
  */
-public class MatriculaDAO {
+public class MatriculaDAO  implements ICrud<Matricula>  {
     
+    private final MongoCollection<Document> coleccion;
+
+    public MatriculaDAO() {
+        MongoDatabase db = ConexionMongo.getInstancia().getDatabase();
+        this.coleccion = db.getCollection("matriculas");
+    }
+
+
+    @Override
+    public boolean crear(Matricula m) {
+        try {
+            Document doc = new Document("idMatricula", m.getIdMatricula())
+                    .append("fecha", m.getFecha())
+                    .append("cedulaEstudiante", m.getCedulaEstudiante())
+                    .append("codigosAsignaturas", m.getCodigosAsignaturas());
+            coleccion.insertOne(doc);
+            return true;
+        } catch (Exception e) {
+            System.err.println("Error al matricular: " + e.getMessage());
+            return false;
+        }
+    }
+    public List<Matricula> listar() {
+        List<Matricula> lista = new ArrayList<>();
+        return lista;
+    }
+
+    @Override
+    public List<Matricula> leerTodos() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+
+    @Override
+    public boolean eliminar(String id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Matricula buscarPorId(String id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean actualizar(Matricula objeto) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
 }
+
