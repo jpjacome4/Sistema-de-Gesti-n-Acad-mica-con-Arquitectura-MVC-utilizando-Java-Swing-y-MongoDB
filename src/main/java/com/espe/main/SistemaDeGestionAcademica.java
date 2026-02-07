0d386117fc3f938aad840cebd9cab0a3c1ce35b1
Jonathan_Jacome <jpjacome4@espe.edu.ec>
2026-02-07 15:14:29 -0500

    Main

--- src/main/java/com/espe/main/SistemaDeGestionAcademica.java
+++ src/main/java/com/espe/main/SistemaDeGestionAcademica.java
@@ -1,9 +1,14 @@
 package com.espe.main;
 
+import com.espe.vista.VistaPrincipal;
 
 public class SistemaDeGestionAcademica {
-
     public static void main(String[] args) {
-        System.out.println("Hola mundo");
+        // Ejecutamos la interfaz gráfica en el hilo de eventos de Swing
+        java.awt.EventQueue.invokeLater(() -> {
+            VistaPrincipal principal = new VistaPrincipal();
+            principal.setLocationRelativeTo(null); // Esto centra la ventana en tu pantalla
+            principal.setVisible(true);
+        });
     }
 }
